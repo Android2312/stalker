5c98ddd207c6759e67194654f066bb207e013476
Android2312 <109897849+Android2312@users.noreply.github.com>
2023-06-10 17:21:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -108,3 +108,16 @@ function close(){
     });
     condition=true
 };
+
+let links =document.querySelectorAll('.scroll');
+let targetID;
+links.forEach(function(element){
+    element.addEventListener('click',function(event){
+        event.preventDefault();
+        targetID = element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior:'smooth',
+            block:'start'
+        })
+    })
+})
